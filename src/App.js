0621e065c60eece309af70acd5f63675bc8cea8a
NikolaiGavrilov <nikolaisibir@mail.ru>
2024-11-13 20:04:58 +0300

    Added shop website files from my GeekBrains repository with my React homeworks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  NavLink,
+  Route,
+  Router,
+  Routes,
+} from "react-router-dom";
+import CartPage from './components/CartPage/CartPage';
+import CatalogPage from './components/CatalogPage/CatalogPage';
+import MainPage from './components/MainPage/MainPage';
+import ProductPage from './components/ProductPage/ProductPage';
+import CheckoutPage from './components/CheckoutPage/CheckoutPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}/>
+          <Route path="/cart" element={<CartPage />}/>
+          <Route path="/catalogue" element={<CatalogPage />}/>
+          <Route path="/product" element={<ProductPage />}/>
+          <Route path="/checkout" element={<CheckoutPage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
